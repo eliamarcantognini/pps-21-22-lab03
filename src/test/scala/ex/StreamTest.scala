4bcbd1d481af58b9ceadf70370b663c7196deb15
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-15 18:23:52 +0100

    Add drop function and its test

--- src/test/scala/ex/StreamTest.scala
+++ src/test/scala/ex/StreamTest.scala
@@ -3,8 +3,15 @@ package ex
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import Stream.*
+import List.*
 
 class StreamTest:
+
   import Stream.*
-  
-  
+  import List.*
+
+  val s: Stream[Int] = Stream.take(Stream.iterate(0)(_ + 1))(10)
+
+  @Test
+  def testDrop(): Unit =
+    assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
