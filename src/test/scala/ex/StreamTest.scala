e42d16121678fb19d73c26d926003d48e9c21b56
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-03-15 19:10:12 +0100

    Refactor

--- src/test/scala/ex/StreamTest.scala
+++ src/test/scala/ex/StreamTest.scala
@@ -10,18 +10,17 @@ class StreamTest:
   import Stream.*
   import List.*
 
-  val s: Stream[Int] = Stream.take(Stream.iterate(0)(_ + 1))(10)
-
   @Test
   def testDrop(): Unit =
+    val s: Stream[Int] = Stream.take(Stream.iterate(0)(_ + 1))(10)
     assertEquals(Cons(6, Cons(7, Cons(8, Cons(9, Nil())))), Stream.toList(Stream.drop(s)(6)))
 
   @Test
   def testConstant(): Unit =
-    val s1 = Cons("x", Cons("x", Cons("x", Cons("x", Cons("x", Nil())))))
-    assertEquals(s1, Stream.toList(Stream.take(constant("x"))(5)))
+    val s = Cons("x", Cons("x", Cons("x", Cons("x", Cons("x", Nil())))))
+    assertEquals(s, Stream.toList(Stream.take(constant("x"))(5)))
 
   @Test
   def testFib(): Unit =
-    val s1 = Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Cons(8, Cons(13 , Nil()))))))))
-    assertEquals(s1, Stream.toList(Stream.take(fib())(8)))
+    val s = Cons(0, Cons(1, Cons(1, Cons(2, Cons(3, Cons(5, Cons(8, Cons(13 , Nil()))))))))
+    assertEquals(s, Stream.toList(Stream.take(fib())(8)))
